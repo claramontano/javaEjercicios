99f65a8c5242b5c0e24b967ec02e3e5a211b9f2d
Clara Montano Rodríguez <montanorodriguezclara@gmail.com>
2025-12-07 20:06:20 +0100

    Añadido banner y ejercicio ex11

---
+++ src/level1_beginner/ex11_ex20_condicionales_bucles/ex11ImprimirNumerosDel1Al100.java
@@ -0,0 +1,26 @@
+package level1_beginner.ex11_ex20_condicionales_bucles;
+
+/** 
+____________________________________________________________
+|															|
+|	Ejercicio 11: Imprimir los números del 1 al 100.		|
+|	Nivel: Beginner											|
+|	Tema: Fundamentos y Variables							|
+|	Autor: Clara Montaño Rodríguez							|
+|	Fecha: 07/12/2025										|
+|___________________________________________________________|
+
+*/
+
+public class ex11ImprimirNumerosDel1Al100 {
+
+	public static void main(String[] args) {
+		
+		//Imprime los números del 1 al 100 usando un bucle for
+		//En cada iteración el contador i aumenta en 1 y se imprime su valor.
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
